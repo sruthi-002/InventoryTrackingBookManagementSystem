9584fb31b8d193267dc5a0519399a98ec9187409
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-24 00:52:42 +0530

    Update BookRepo.java

--- src/main/java/com/example/demo/Repo/BookRepo.java
+++ src/main/java/com/example/demo/Repo/BookRepo.java
@@ -8,10 +8,10 @@ import com.example.demo.model.Book;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book,Integer>{
-	@Query(value="SELECT * FROM InventoryTrackingSystem as b WHERE (b.title like '%?:keyword%' OR b.author like'%:keyword%' OR b.id like '%:keyword%') AND b.category like '%:category%")
+	@Query(value="SELECT * FROM InventoryTrackingSystem as b WHERE (b.title like '%?:keyword%' OR b.author like'%:keyword%' OR b.id like '%:keyword%') AND b.category like '%:category%",nativeQuery=true)
 	List<Book> findBookByKeywordAndCategory(String keyword , String category);
 	@Query(value = "Select IF(SUM(b.sold) IS NULL,0,SUM(b.sold)) from book b where " +
             "(b.title like %?1% OR CAST(b.id as CHAR) like %?1% OR LOWER(b.author) like %?1%) " +
-            "AND b.category=?2 AND b.sold>0")
+            "AND b.category=?2 AND b.sold>0",nativeQuery=true)
     int countNumberOfBooksSold(String keyword, String category);
 }
